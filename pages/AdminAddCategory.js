2a3e70227d694ff5b1c783cf97dfcd72c9df4c1c
wajid90 <wajid90273@gmail.com>
2023-08-21 02:02:34 -0400

    order page

--- pages/AdminAddCategory.js
+++ pages/AdminAddCategory.js
@@ -1,9 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Platform, StatusBar } from 'react-native'
 import React from 'react'
 
 const AdminAddCategory = () => {
   return (
-    <View>
+    <View
+    style={{
+      paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      backgroundColor: "white",
+      flex:1
+    }} 
+    >
       <Text>AdminAddCategory</Text>
     </View>
   )
